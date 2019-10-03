c90ecdb04ed5721c12884ea3c40d8ce90ea83996
cvevera <cvevera@gmail.com>
2019-10-02 23:05:41 -0400

    worked out a problem where randomNumbers was 0 on 'click'

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,32 @@
 
-var randomNumber= "";
-var total= "";
-var crystalOne= 0;
-var crystalTwo= 0;
-var crystalThree= 0;
-var crystalFour= 0;
+// var randomNumber= "";
+// var total= "";
+// var crystalOne= 0;
+// var crystalTwo= 0;
+// var crystalThree= 0;
+// var crystalFour= 0;
 
 
 
-// between 19-120
-    function randomIntFromInterval(min, max) { // min and max included 
-        return Math.floor(Math.random() * (max - min + 1) + min);
-      }
+// // // between 19-120
+// //     function randomIntFromInterval(min, max) { // min and max included 
+// //         return Math.floor(Math.random() * (max - min + 1) + min);
+// //       }
 
 
 
-function newgame(){
-    var randomNumber = randomIntFromInterval(19,120)
-    $(".target-number").text("Target: " + randomNumber);
-    console.log(randomNumber);
-    total = 0;
-    $(".total-score-is").text("Your total score is" + total);
-}; 
+// // function newgame(){
+// //     var randomNumber = randomIntFromInterval(19,120)
+// //     $(".target-number").text("Target: " + randomNumber);
+// //     console.log(randomNumber);
+// //     total = 0;
+// //     $(".total-score-is").text("Your total score is" + total);
+// // }; 
 
 
-newgame()
+// // newgame()
 
 
-$("#crystal1").click(function (){
-    alert("clicked")
-});
+// // $("#crystal1").click(function (){
+// //     alert("clicked")
+// // });
